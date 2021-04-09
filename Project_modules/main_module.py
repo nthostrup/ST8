aa5a94fc1db35c26488915f3ef320831d94a9529
Maria <mmalle17@student.aau.dk>
2021-04-09 10:18:16 +0200

    Dice similarity implemented

--- Project_modules/main_module.py
+++ Project_modules/main_module.py
@@ -24,8 +24,8 @@ class main_class:
         self.TEST_INPUT_DIR = test_input_dir
         self.TEST_MASK_DIR = test_mask_dir
         self.BATCH_SIZE = 20
-        self.EPOCHS = 2
-        self.IMG_SIZE = (512,512)
+        self.EPOCHS = 15
+        self.IMG_SIZE = (512, 512)
         self.NUM_CHANNELS_OUT = 1
 
         self.SAMPLES_TO_RUN = 100
@@ -75,20 +75,22 @@ class main_class:
         #Plot history of training
         utils.plot_training_history(history)
 
+
         #Load model from directory
         #model = load_model("Unet_MRI-1.h5")#Load
 
         #Validate model and plot image, mask and prediction
-        predictions = model_module.test_model(model, valid_gen)
+        predictions, f1_score = model_module.test_model(model, valid_gen)   # https://datascience.stackexchange.com/questions/45165/how-to-get-accuracy-f1-precision-and-recall-for-a-keras-model
+        print('The dice similarity score is:', f1_score)
 
         #Plot predictions
         utils.plot_predictions(predictions, validation_img_paths, validation_mask_paths)
 
-    def train_model(self,model, train_generator, validation_generator, epochs):
+    def train_model(self, model, train_generator, validation_generator, epochs):
         #Compile model dependant on output dimensions
         #TODO: Implement metric DICE
         #Metric MeanIoU:  IOU is defined as follows: IOU = true_positive / (true_positive + false_positive + false_negative
-        model.compile(optimizer="adam", loss="binary_crossentropy", metrics = [keras.metrics.MeanIoU(num_classes=2)])#Works with 2 classes as output from model.
+        model.compile(optimizer="adam", loss="binary_crossentropy", metrics=['acc', utils.f1_m, utils.precision_m, utils.recall_m])#Works with 2 classes as output from model.
         model.summary()
 
 
--- Project_modules/model_module.py
+++ Project_modules/model_module.py
@@ -57,45 +57,17 @@ def get_model(img_size, num_classes):
 
     return model
 
-
-#Train model
-def train_model(model, train_generator, validation_generator, epochs):
-    #Compile model dependant on output dimensions
-    #TODO: Implement metric DICE
-    #Metric MeanIoU:  IOU is defined as follows: IOU = true_positive / (true_positive + false_positive + false_negative
-    model.compile(optimizer="adam", loss="binary_crossentropy", metrics = [keras.metrics.MeanIoU(num_classes=2)])#Works with 2 classes as output from model.
-    model.summary()
-    
-    earlystopper = EarlyStopping(monitor='val_loss',patience=5, verbose=1)
-    checkpointer = ModelCheckpoint('model-checkpoint_Unet_MRI.h5', monitor='val_loss', verbose=1, save_best_only=True)
-    callback = [earlystopper, checkpointer]
-
-    # Train the model, doing validation at the end of each epoch.
-    history = model.fit(train_generator, epochs=epochs, validation_data=validation_generator, callbacks=callback)#Training with validation as generator or dataset
-    #history = model.fit(train_generator, epochs=epochs, validation_data=validation_data, validation_batch_size=val_batch_size, callbacks=callback)#Training with validation as tuple
-    
-    
-    #history = model.fit(train_generator, epochs=epochs, callbacks=callback)#Training without validation
-    
-    
-    #Save model
-    #model.save('Unet_MRI-2.h5')
-    
-    return history, model
-
 #Test model with test data(or other data) and plot prediction vs. image vs. true mask
 def test_model(model,test_generator):
     #TODO: Maybe calculate dice based on prediction and ground truth
         
     #TODO: Evaluate with some sample images (x_test) and ground truth masks (y_test)
     #result = model.evaluate(x_test,y_test, batch_size=32)
+    loss, accuracy, f1_score, precision, recall = model.evaluate(test_generator, verbose=1)
+
+    predictions = model.predict(test_generator, steps=1)
+
     
     
-    predictions = model.predict(test_generator, steps = 1)
-    
-    
-    
-    
-    
-    return predictions
+    return predictions, f1_score
 
